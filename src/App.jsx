2a178f1b608ac30d8f14824decbe988f18a8f425
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-25 17:17:19 +0545

    fix path in app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import MainLayout from './layout/MainLayout';
-import Dashboard from './pages/Dashboard';
-import Board from './pages/Board';
-import LoadingSpinner from './components/LoadingSpinner';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import MainLayout from "./layout/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Board from "./pages/Board";
+import LoadingSpinner from "./components/LoadingSpinner";
+import NavBar from "./components/auth/Navbar";
 
 function App() {
   const { isLoading, isAuthenticated } = useAuth0();
@@ -15,18 +15,43 @@ function App() {
   }
 
   return (
-    
-      <Routes>
-        <Route path="/" element={
-          isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
-        } />
-        <Route path="/dashboard" element={
-          isAuthenticated ? <MainLayout><Dashboard /></MainLayout> : <Navigate to="/" />
-        } />
-        <Route path="/board/:boardId" element={
-          isAuthenticated ? <MainLayout><Board /></MainLayout> : <Navigate to="/" />
-        } />
-      </Routes>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/dashboard" />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          isAuthenticated ? (
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
+      <Route
+        path="/board/:boardId"
+        element={
+          isAuthenticated ? (
+            <MainLayout>
+              <Board />
+            </MainLayout>
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
+      <Route path="/login" element={<NavBar />} />
+    </Routes>
   );
 }
 
